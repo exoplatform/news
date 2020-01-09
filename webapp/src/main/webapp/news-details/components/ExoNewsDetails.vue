57f03214dbc72f8de1feb5ab9f9e7b79f20f1a70
Rdidmohamedelhadi <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2020-01-09 15:22:36 +0100

    Pin icon is disabled from details when the article is archived (#144)

--- webapp/src/main/webapp/news-details/components/ExoNewsDetails.vue
+++ webapp/src/main/webapp/news-details/components/ExoNewsDetails.vue
@@ -12,7 +12,7 @@
       <div class="newsDetails-header">
         <exo-news-share-activity v-if="showShareButton" :activity-id="activityId" :news-id="newsId" :news-title="news.title" :news-archived="news.archived"></exo-news-share-activity>
         <exo-news-activity-edit-composer v-if="showEditButton" :news-id="newsId" :activity-id="activityId"></exo-news-activity-edit-composer>
-        <exo-news-pin-activity v-if="showPinInput" :news-id="newsId" :news-pinned="news.pinned" :news-title="news.title"></exo-news-pin-activity>
+        <exo-news-pin-activity v-if="showPinInput" :news-id="newsId" :news-pinned="news.pinned" :news-archived="news.archived" :news-title="news.title"></exo-news-pin-activity>
         <exo-news-archive v-if="news.archived" :news-id="newsId" :news-archived="news.archived" :news-title="news.title" :pinned="news.pinned" @update-archived-field="updateArchivedField"></exo-news-archive>
         
         <div class="newsDetails">

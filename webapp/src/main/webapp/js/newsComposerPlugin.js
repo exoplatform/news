3f8e266b051852aa0ea0ed632f585991fb4e00ea
Rdidmohamedelhadi <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2020-04-30 12:33:35 +0100

    Display 'Switch to an article' when the number of caracters exceeds 1300 (#218)

--- webapp/src/main/webapp/js/newsComposerPlugin.js
+++ webapp/src/main/webapp/js/newsComposerPlugin.js
@@ -25,5 +25,28 @@
     }
   };
 
+  const switchToArticleActivityComposerPlugin = {
+    key: 'switchToArticle',
+    rank: 10,
+    resourceBundle: 'locale.portlet.news.News',
+    labelKey: 'news.composer.switch.article',
+
+    onExecute: function() {
+      let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
+      if(eXo.env.portal.spaceId) {
+        url += `?spaceId=${eXo.env.portal.spaceId}`
+      }
+
+      const editor = CKEDITOR.instances['activityContent'];
+      if(editor) {
+        const message = CKEDITOR.instances['activityContent'].getData();
+        localStorage.setItem('exo-activity-composer-message', message);
+      }
+
+      window.open(url,'_blank');
+    }
+  }
+
   extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-action', newsActivityComposerPlugin);
+  extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-hint-action', switchToArticleActivityComposerPlugin);
 })(extensionRegistry);

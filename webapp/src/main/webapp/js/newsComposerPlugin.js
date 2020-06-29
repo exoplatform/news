8d0c0521b69d473b4f5bb6e054b108b30224de93
aelbenney <42932906+aelbenney@users.noreply.github.com>
2020-06-29 16:06:37 +0100

    Feature/news: Restrict displaying news composer plugin for redactors only (#242)
    * restrict displaying news composer plugin for redactors only

--- webapp/src/main/webapp/js/newsComposerPlugin.js
+++ webapp/src/main/webapp/js/newsComposerPlugin.js
@@ -6,12 +6,15 @@ const newsActivityComposerPlugin = {
   description: 'news.composer.write.description',
   iconClass: 'newsComposerIcon',
   enabled: function() {
-    return eXo.env.portal.spaceId != null && eXo.env.portal.spaceId.length !== 0;
+    let isRedactor = false;
+    isRedactor = getSpaceMemberships(`/spaces/${eXo.env.portal.spaceGroup}`);
+    return eXo.env.portal.spaceId != null && eXo.env.portal.spaceId.length !== 0 && isRedactor;
+
   },
   onExecute: function() {
     let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
     if(eXo.env.portal.spaceId) {
-      url += `?spaceId=${eXo.env.portal.spaceId}`
+      url += `?spaceId=${eXo.env.portal.spaceId}`;
     }
 
     const editor = CKEDITOR.instances['activityContent'];
@@ -33,7 +36,7 @@ const switchToArticleActivityComposerPlugin = {
   onExecute: function(attachments) {
     let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
     if(eXo.env.portal.spaceId) {
-      url += `?spaceId=${eXo.env.portal.spaceId}`
+      url += `?spaceId=${eXo.env.portal.spaceId}`;
     }
 
     const editor = CKEDITOR.instances['activityContent'];
@@ -45,8 +48,24 @@ const switchToArticleActivityComposerPlugin = {
 
     window.open(url,'_blank');
   }
-}
+};
 require(['SHARED/extensionRegistry'], function (extensionRegistry) {
   extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-action', newsActivityComposerPlugin);
   extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-hint-action', switchToArticleActivityComposerPlugin);
 });
+
+function getSpaceMemberships(spaceGroupId) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/groups/memberships?groupId=${spaceGroupId}`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
+  }).then((resp) => resp.json()).then(memberships => {
+    const redactors = memberships.entities.filter(membership => {
+      return membership.membershipType === 'redactor';
+    });
+    const spaceHasRedactors = !!redactors;
+    const isCurrentUserRedactor = redactors.some(membership => membership.userName === eXo.env.portal.userName);
+    return !spaceHasRedactors || isCurrentUserRedactor;
+  });
+}

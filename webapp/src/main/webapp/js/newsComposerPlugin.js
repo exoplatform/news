7a02dc6708241455bd0d9deb91399ea35b2bd752
aelbenney <42932906+aelbenney@users.noreply.github.com>
2020-07-07 12:05:08 +0100

    MAINT-26328 : Feature/news: Create rest service in news to enable/disable news composer plugin (#244) (#19)

--- webapp/src/main/webapp/js/newsComposerPlugin.js
+++ webapp/src/main/webapp/js/newsComposerPlugin.js
@@ -5,26 +5,20 @@ const newsActivityComposerPlugin = {
   labelKey: 'news.composer.write',
   description: 'news.composer.write.description',
   iconClass: 'newsComposerIcon',
-  enabled: function() {
-    let isRedactor = false;
-    if (eXo.env.portal.spaceId) {
-      isRedactor = getSpaceMemberships(`/spaces/${eXo.env.portal.spaceGroup}`);
-    }
-    return eXo.env.portal.spaceId != null && eXo.env.portal.spaceId.length !== 0 && isRedactor;
-  },
-  onExecute: function() {
+  enabled: false,
+  onExecute: function () {
     let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
-    if(eXo.env.portal.spaceId) {
+    if (eXo.env.portal.spaceId) {
       url += `?spaceId=${eXo.env.portal.spaceId}`;
     }
 
     const editor = CKEDITOR.instances['activityContent'];
-    if(editor) {
+    if (editor) {
       const message = CKEDITOR.instances['activityContent'].getData();
       localStorage.setItem('exo-activity-composer-message', message);
     }
 
-    window.open(url,'_blank');
+    window.open(url, '_blank');
   }
 };
 
@@ -34,39 +28,44 @@ const switchToArticleActivityComposerPlugin = {
   resourceBundle: 'locale.portlet.news.News',
   labelKey: 'news.composer.switch.article',
 
-  onExecute: function(attachments) {
+  onExecute: function (attachments) {
     let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
-    if(eXo.env.portal.spaceId) {
+    if (eXo.env.portal.spaceId) {
       url += `?spaceId=${eXo.env.portal.spaceId}`;
     }
 
     const editor = CKEDITOR.instances['activityContent'];
-    if(editor) {
+    if (editor) {
       const message = CKEDITOR.instances['activityContent'].getData();
       localStorage.setItem('exo-activity-composer-message', message);
       localStorage.setItem('exo-activity-composer-attachments', JSON.stringify(attachments));
     }
 
-    window.open(url,'_blank');
+    window.open(url, '_blank');
   }
 };
+
 require(['SHARED/extensionRegistry'], function (extensionRegistry) {
-  extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-action', newsActivityComposerPlugin);
   extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-hint-action', switchToArticleActivityComposerPlugin);
+  document.dispatchEvent(new CustomEvent('activity-composer-extension-updated'));
+  if (eXo.env.portal.spaceId) {
+    canUserCreateNews().then(canCreateNews => {
+      newsActivityComposerPlugin.enabled = eXo.env.portal.spaceId && (canCreateNews === 'true');
+      extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-action', newsActivityComposerPlugin);
+      document.dispatchEvent(new CustomEvent('activity-composer-extension-updated'));
+    });
+  } else {
+    newsActivityComposerPlugin.enabled = false;
+  }
 });
 
-function getSpaceMemberships(spaceGroupId) {
-  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/groups/memberships?groupId=${spaceGroupId}`, {
+function canUserCreateNews() {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/news/canCreateNews/${eXo.env.portal.spaceId}`, {
     headers: {
       'Content-Type': 'application/json'
     },
     method: 'GET'
-  }).then((resp) => resp.json()).then(memberships => {
-    const redactors = memberships.entities.filter(membership => {
-      return membership.membershipType === 'redactor';
-    });
-    const spaceHasRedactors = !!redactors;
-    const isCurrentUserRedactor = redactors.some(membership => membership.userName === eXo.env.portal.userName);
-    return !spaceHasRedactors || isCurrentUserRedactor;
+  }).then((resp) => resp.text()).then(resp => {
+    return resp;
   });
 }

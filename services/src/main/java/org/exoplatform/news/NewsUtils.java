b18aed1b0eec23e0d4d3ca6441d395a265163455
aelbenney <42932906+aelbenney@users.noreply.github.com>
2021-02-05 12:56:11 +0100

    TASK-39258: Add notification when being mentioned in an article (#69)

--- services/src/main/java/org/exoplatform/news/NewsUtils.java
+++ services/src/main/java/org/exoplatform/news/NewsUtils.java
@@ -5,11 +5,16 @@ import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.commons.utils.HTMLEntityEncoder;
 import org.exoplatform.commons.utils.HTMLSanitizer;
 import org.exoplatform.news.model.News;
+import org.exoplatform.news.notification.plugin.MentionInNewsNotificationPlugin;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 import org.exoplatform.social.core.manager.IdentityManager;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
 
 public class NewsUtils {
 
@@ -63,5 +68,36 @@ public class NewsUtils {
       return null;
     }
     return identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username);
+  }
+
+  /**
+   * Processes Mentioners who has been mentioned via the news body.
+   *
+   * @param body
+   * @return set of mentioned users
+   */
+  public static Set<String> processMentions(String body) {
+    Set<String> mentions = new HashSet<>();
+    mentions.addAll(parseMention(body));
+
+    return mentions;
+  }
+
+  private static Set<String> parseMention(String str) {
+    if (str == null || str.length() == 0) {
+      return Collections.emptySet();
+    }
+
+    Set<String> mentions = new HashSet<>();
+    Matcher matcher = MentionInNewsNotificationPlugin.MENTION_PATTERN.matcher(str);
+    while (matcher.find()) {
+      String remoteId = matcher.group().substring(1);
+      Identity identity = loadUser(remoteId);
+      // if not the right mention then ignore
+      if (identity != null && !mentions.contains(identity.getRemoteId())) {
+        mentions.add(identity.getRemoteId());
+      }
+    }
+    return mentions;
   }
 }

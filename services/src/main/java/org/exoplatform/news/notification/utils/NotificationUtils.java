63cd831674043e5d11fab3a3febe4e1ed4f2d905
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2021-07-09 18:34:15 +0100

    update NotificationUtils

--- services/src/main/java/org/exoplatform/news/notification/utils/NotificationUtils.java
+++ services/src/main/java/org/exoplatform/news/notification/utils/NotificationUtils.java
@@ -9,10 +9,14 @@ import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.news.model.News;
 import org.exoplatform.services.jcr.RepositoryService;
+import org.exoplatform.services.jcr.ext.app.SessionProviderService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.User;
 import org.exoplatform.services.organization.UserHandler;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.service.LinkProvider;
 import org.exoplatform.social.core.space.model.Space;
 
@@ -29,7 +33,8 @@ public class NotificationUtils {
   }
 
   public static String getNewsIllustration(News news) throws Exception {
-    SessionProvider sessionProvider = SessionProvider.createSystemProvider();
+    SessionProviderService sessionProviderService = CommonsUtils.getService(SessionProviderService.class);
+    SessionProvider sessionProvider = sessionProviderService.getSessionProvider(null);
     RepositoryService repositoryService = CommonsUtils.getService(RepositoryService.class);
     Session session = sessionProvider.getSession(
                                                  repositoryService.getCurrentRepository()

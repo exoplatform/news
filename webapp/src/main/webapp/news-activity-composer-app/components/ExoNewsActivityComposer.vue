52a424a25ff2da716070b2c1337fa95c89f83914
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2021-08-19 15:54:33 +0100

    MAINT-47231: Allow saving inserted and dropped images in news body to be saved in draft (#248)
    (cherry picked from commit cece48c959eea0593ebbe4d96f85cf71dd05558c)
    (cherry picked from commit 8b41d8e891a2214a6156703cb0fd8a36d177b5fc)

--- webapp/src/main/webapp/news-activity-composer-app/components/ExoNewsActivityComposer.vue
+++ webapp/src/main/webapp/news-activity-composer-app/components/ExoNewsActivityComposer.vue
@@ -433,6 +433,13 @@ export default {
           },
           change: function (evt) {
             self.news.body = evt.editor.getData();
+            self.autoSave();
+          },
+          drop: function (evt) {
+            window.setTimeout(() => {
+              self.news.body = evt.editor.getData();
+              self.autoSave();
+            }, 1000);
           }
         }
       });

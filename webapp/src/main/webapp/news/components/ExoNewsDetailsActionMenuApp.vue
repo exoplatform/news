742fbb8839f200dfdecddab83db01212909d8e17
Hassen El kamel <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-09-24 14:09:38 +0100

    Task-49368: Fix regression on edit/delete/resume articles from news app (#288)
    Prior to this change, after refactoring done in mobile view `ExoNewsDetailsActionMenu` isn't the child component of NewsApp, the deletion and edition action doesn't work. So to make this action possible, we have created a child component `ExoNewsDetailsActionMenuApp` allowing edition and deletion.

---
+++ webapp/src/main/webapp/news/components/ExoNewsDetailsActionMenuApp.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-menu
+    v-model="actionMenu"
+    eager
+    bottom
+    left
+    offset-y
+    min-width="108px"
+    class="px-0 text-right mx-2">
+    <template #activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        class="newsDetailsActionMenu pull-right"
+        icon
+        v-on="on">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item v-if="showEditButton" @click="$emit('edit')">
+        <v-list-item-title>
+          {{ $t('news.details.header.menu.edit') }}
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item v-if="showShareButton && news.activityId" @click="$root.$emit('activity-share-drawer-open', news.activityId)">
+        <v-list-item-title>
+          {{ $t('news.details.header.menu.share') }}
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item v-if="showResumeButton" @click="$emit('edit')">
+        <v-list-item-title>
+          {{ $t('news.details.header.menu.resume') }}
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item v-if="showDeleteButton" @click="$emit('delete')">
+        <v-list-item-title>
+          {{ $t('news.details.header.menu.delete') }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    news: {
+      type: Object,
+      required: false,
+      default: null
+    },
+    showShareButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    showEditButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    showResumeButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    showDeleteButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+  data: () => ({
+    actionMenu: null,
+  }),
+  mounted() {
+    $('#UIPortalApplication').parent().click(() => {
+      this.actionMenu = false;
+    });
+  },
+};
+</script>

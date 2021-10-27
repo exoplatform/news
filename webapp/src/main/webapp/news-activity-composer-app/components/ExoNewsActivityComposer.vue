a57e4d9c62f86a614d2e3b11440f15f232be9be0
hakermi <hakermi@exoplatform.com>
2021-10-27 10:18:08 +0100

    MAINT-50429: Fix updating inserted images when editing an article
    ISSUE: When editing the news body there was no save of images urls diff and were always set to the default composer url image
    before processing the images.
    FIX: Updating the images Urls diff when editing an article and processing the images.

--- webapp/src/main/webapp/news-activity-composer-app/components/ExoNewsActivityComposer.vue
+++ webapp/src/main/webapp/news-activity-composer-app/components/ExoNewsActivityComposer.vue
@@ -916,7 +916,12 @@ export default {
         updatedNews.uploadId = '';
       }
 
-      return this.$newsServices.updateNews(updatedNews).then(() => this.draftSavingStatus = this.$t('news.composer.draft.savedDraftStatus'));
+      return this.$newsServices.updateNews(updatedNews).then((createdNews) => {
+        if (this.news.body !== createdNews.body) {
+          this.imagesURLs = this.extractImagesURLsDiffs(this.news.body, createdNews.body);
+        }
+      }).then(() => this.$emit('draftUpdated'))
+        .then(() => this.draftSavingStatus = this.$t('news.composer.draft.savedDraftStatus'));
     },
     goBack() {
       if ( history.length > 1) {

1535537863e8e3b6c2562adff2cda30dac7bb705
Hassen El kamel <54801416+hassenelkamel-exo@users.noreply.github.com>
2021-11-23 16:25:16 +0100

    Task-50926: Implement vuejs news list portlet prototype (#343)
    Prior to this change, we use old CLV news portlets in snapshot page. After this change, we add new vuejs news list portlet with a NewsSettings drawer which allows to choose news target and view template to use for displaying the articles.

---
+++ webapp/src/main/webapp/news-list-view/js/NewsListService.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {newsConstants} from '../../js/newsConstants.js';
+
+export function getNewsList(newsTarget, limit) { // eslint-disable-line no-unused-vars
+  // TODO
+  return Promise.resolve([]);
+}
+
+export function getNewsTarget() {
+  return fetch(`${newsConstants.NEWS_API}/targeting`, {
+    credentials: 'include',
+    method: 'GET',
+  }).then((resp) => {
+    if (resp && resp.ok) {
+      return resp.json();
+    }
+  });
+}
+
+export function saveSettings(saveSettingsURL, settings) {
+  const formData = new FormData();
+  if (settings) {
+    Object.keys(settings).forEach(name => {
+      formData.append(name, settings[name]);
+    });
+  }
+  return fetch(saveSettingsURL, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams(formData).toString(),
+  }).then(resp => {
+    if (!resp || !resp.ok) {
+      throw new Error('Response code indicates a server error', resp);
+    }
+  });
+}

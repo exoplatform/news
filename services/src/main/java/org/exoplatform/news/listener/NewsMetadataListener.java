cb5d244db180c910c7c51db1e4e9110018fe33e0
Anas Elbenney <42932906+aelbenney@users.noreply.github.com>
2021-12-08 16:43:46 +0100

    TASK-51421: Bookmark/unbookmark a news

---
+++ services/src/main/java/org/exoplatform/news/listener/NewsMetadataListener.java
@@ -0,0 +1,73 @@
+package org.exoplatform.news.listener;
+
+import org.apache.commons.lang3.StringUtils;
+import org.exoplatform.commons.search.index.IndexingService;
+import org.exoplatform.news.model.News;
+import org.exoplatform.news.search.NewsIndexingServiceConnector;
+import org.exoplatform.news.service.NewsService;
+import org.exoplatform.news.utils.NewsUtils;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.services.security.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.processor.MetadataActivityProcessor;
+import org.exoplatform.social.metadata.favorite.FavoriteService;
+import org.exoplatform.social.metadata.favorite.model.Favorite;
+import org.exoplatform.social.metadata.model.MetadataItem;
+
+public class NewsMetadataListener extends Listener<Long, MetadataItem> {
+
+  private final IndexingService indexingService;
+
+  private final NewsService     newsService;
+
+  private final FavoriteService favoriteService;
+
+  private final IdentityManager identityManager;
+
+  private static final String   METADATA_CREATED = "social.metadataItem.created";
+
+  private static final String   METADATA_DELETED = "social.metadataItem.deleted";
+
+  public NewsMetadataListener(IndexingService indexingService,
+                              NewsService newsService,
+                              FavoriteService favoriteService,
+                              IdentityManager identityManager) {
+    this.indexingService = indexingService;
+    this.newsService = newsService;
+    this.favoriteService = favoriteService;
+    this.identityManager = identityManager;
+  }
+
+  @Override
+  public void onEvent(Event<Long, MetadataItem> event) throws Exception {
+    ConversationState conversationstate = ConversationState.getCurrent();
+    Identity currentIdentity = conversationstate == null
+        || conversationstate.getIdentity() == null ? null : conversationstate.getIdentity();
+    MetadataItem metadataItem = event.getData();
+    String objectType = event.getData().getObjectType();
+    String objectId = metadataItem.getObjectId();
+    if (StringUtils.equals(objectType, NewsUtils.NEWS_METADATA_OBJECT_TYPE)) {
+      indexingService.reindex(NewsIndexingServiceConnector.TYPE, objectId);
+    } else if (StringUtils.equals(objectType, MetadataActivityProcessor.ACTIVITY_METADATA_OBJECT_TYPE)) {
+      News news = newsService.getNewsByActivityId(objectId, currentIdentity);
+      if (currentIdentity != null) {
+        org.exoplatform.social.core.identity.model.Identity userIdentity =
+                                                                         identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME,
+                                                                                                             currentIdentity.getUserId());
+        Favorite favorite = new Favorite(NewsUtils.NEWS_METADATA_OBJECT_TYPE,
+                                         news.getId(),
+                                         "",
+                                         Long.parseLong(userIdentity.getId()));
+        if (event.getEventName().equals(METADATA_CREATED)) {
+          favoriteService.createFavorite(favorite);
+        } else if (event.getEventName().equals(METADATA_DELETED)) {
+          favoriteService.deleteFavorite(favorite);
+        }
+      }
+      indexingService.reindex(NewsIndexingServiceConnector.TYPE, news.getId());
+    }
+  }
+}

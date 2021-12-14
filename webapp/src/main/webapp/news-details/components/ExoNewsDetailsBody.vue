cd11fedb8119d3687d16d3c28f47a4116b7d006d
saraboutej <sboutej@exoplatform.com>
2021-12-14 15:31:46 +0100

    ACC-TASK-52070: Add role presentation for decorative images in news detail page

--- webapp/src/main/webapp/news-details/components/ExoNewsDetailsBody.vue
+++ webapp/src/main/webapp/news-details/components/ExoNewsDetailsBody.vue
@@ -31,7 +31,10 @@
               <div :class="[ showUpdateInfo ? 'newsUpdateInfo' : '']">
                 <div class="activityAvatar avatarCircle">
                   <a :href="authorProfileURL">
-                    <img :src="authorAvatarURL" class="avatar">
+                    <img
+                      :src="authorAvatarURL"
+                      class="avatar"
+                      role="presentation">
                   </a>
                 </div>
               </div>

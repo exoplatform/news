f9fa0e4820d4dcf3e9f81ceddf56ad38faa7fbcc
El kamel Hassen <helkamel@exoplatform.com>
2022-01-07 10:51:16 +0100

    Task-50796: Update some variables names and add sonar feedback

--- webapp/src/main/webapp/news-targeting-settings/main.js
+++ webapp/src/main/webapp/news-targeting-settings/main.js
@@ -1,8 +1,24 @@
+/*
+ * Copyright (C) 2021 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import './initComponents.js';
 
 // get override components if exists
 if (extensionRegistry) {
-  const components = extensionRegistry.loadComponents('newsTargets');
+  const components = extensionRegistry.loadComponents('newsTargetsSettings');
   if (components && components.length > 0) {
     components.forEach(cmp => {
       Vue.component(cmp.componentName, cmp.componentOptions);

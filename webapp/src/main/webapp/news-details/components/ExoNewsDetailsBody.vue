869e6d2f88eb57988521a5ead6f8ec77e21f1a66
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-01-14 09:21:33 +0100

    MAINT-52374: Fix xss in news content details (#407) (#413)
    ISSUE: The news content has an xss vulnerability by using the v-html directive when displaying the news body, also the HTMLSantizer was doing its work and sanitize the body but the StringEscapeUtils.unescapeHtml was
    reverting the sanitization of the svg tag which caused an xss on svg script attributes.
    FIX: Use v-html-sanitize instead to sanitize the news body and prevent svg dangerous attributes from being executed as a script also remove the unescapeHtml on the sanitized body

--- webapp/src/main/webapp/news-details/components/ExoNewsDetailsBody.vue
+++ webapp/src/main/webapp/news-details/components/ExoNewsDetailsBody.vue
@@ -97,7 +97,7 @@
             id="newsBody"
             :class="[!summary ? 'fullDetailsBodyNoSummary' : '']"
             class="fullDetailsBody clearfix">
-            <span v-html="newsBody"></span>
+            <span v-sanitized-html="newsBody"></span>
           </div>
 
           <div v-show="attachments && attachments.length" class="newsAttachmentsTitle">

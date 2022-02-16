9d21c0c74fbc1f54fa5b480060c94f29c3a6f13c
El kamel Hassen <helkamel@exoplatform.com>
2022-02-16 18:01:25 +0100

    Task-53818: Fix undefined problem

--- webapp/src/main/webapp/news-publish-targets-management/components/NewsPublishTargetsManagement.vue
+++ webapp/src/main/webapp/news-publish-targets-management/components/NewsPublishTargetsManagement.vue
@@ -134,7 +134,6 @@ export default {
   methods: {
     init() {
       if (!this.initialized) {
-        this.initializing = true;
         this.loading = true;
         this.$newsTargetingService.getAllTargets()
           .then(newsTargets => {

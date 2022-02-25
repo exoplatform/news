9899bda6574da646e2fa02b447047dd7d9dff21f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-02-25 18:48:54 +0100

    Define Activity Composer Extensions using new plugins (#465)
    This PR will update the activity composer extensions using new plugins

---
+++ webapp/src/main/webapp/news-extensions/components/activity/ActivitySwitchToNews.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="action">
+    <v-btn
+      v-if="displayButton"
+      :href="link"
+      color="primary"
+      target="_blank"
+      class="ps-0"
+      link
+      text
+      @click="switchToNews">
+      <v-icon size="12">fas fa-pencil-alt fa-sm</v-icon>
+      <span class="ms-1 font-weight-bold text-decoration-underline text-capitalize-first-letter">
+        {{ $t('news.composer.switch.article') }}
+      </span>
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    activityId: {
+      type: String,
+      default: null,
+    },
+    message: {
+      type: String,
+      default: null,
+    },
+    maxMessageLength: {
+      type: Number,
+      default: 0,
+    },
+    templateParams: {
+      type: Object,
+      default: null,
+    },
+    files: {
+      type: Array,
+      default: null,
+    },
+  },
+  computed: {
+    link() {
+      const baseUrl = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
+      if (eXo.env.portal.spaceId) {
+        return `${baseUrl}?spaceId=${eXo.env.portal.spaceId}`;
+      } else {
+        return baseUrl;
+      }
+    },
+    displayButton() {
+      return this.message && this.message.length > this.maxMessageLength;
+    },
+  },
+  methods: {
+    switchToNews() {
+      localStorage.setItem('exo-activity-composer-message', this.message || '');
+      localStorage.setItem('exo-activity-composer-attachments', JSON.stringify(this.files || []));
+    },
+  },
+};
+</script>

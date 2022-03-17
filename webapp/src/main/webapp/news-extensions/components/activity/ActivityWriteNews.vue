b4823e3ed2493b5250f6eaf3c670abac5fb748e3
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-03-17 16:31:03 +0100

    Task-55240: Disable write an article feature when a poll is created

--- webapp/src/main/webapp/news-extensions/components/activity/ActivityWriteNews.vue
+++ webapp/src/main/webapp/news-extensions/components/activity/ActivityWriteNews.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="actionItem" @click="switchToNews">
+  <div
+    class="actionItem"
+    @click="switchToNews"
+    :title="disableComposerButtonTooltip">
     <div class="actionItemIcon">
       <div class="newsComposerIcon"></div>
     </div>
@@ -34,16 +37,40 @@ export default {
       type: Array,
       default: null,
     },
+    activityType: {
+      type: Array,
+      default: null,
+    },
+  },
+  computed: {
+    disableComposerButton() {
+      return this.activityType && this.activityType.length !== 0;
+    },
+    disableComposerButtonTooltip() {
+      return this.disableComposerButton ? this.$t('news.composer.write.disable') : '';
+    }
+  },
+  watch: {
+    disableComposerButton(value) {
+      if (value) {
+        document.getElementsByClassName('actionItem')[1].setAttribute('style', 'opacity: 0.5');
+      }
+      else {
+        document.getElementsByClassName('actionItem')[1].removeAttribute('style');
+      }
+    }
   },
   methods: {
     switchToNews() {
-      let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
-      if (eXo.env.portal.spaceId) {
-        url += `?spaceId=${eXo.env.portal.spaceId}`;
+      if (!this.disableComposerButton) {
+        let url = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/news/editor`;
+        if (eXo.env.portal.spaceId) {
+          url += `?spaceId=${eXo.env.portal.spaceId}`;
+        }
+        localStorage.setItem('exo-activity-composer-message', this.message || '');
+        localStorage.setItem('exo-activity-composer-attachments', JSON.stringify(this.files || []));
+        window.open(url, '_blank');
       }
-      localStorage.setItem('exo-activity-composer-message', this.message || '');
-      localStorage.setItem('exo-activity-composer-attachments', JSON.stringify(this.files || []));
-      window.open(url, '_blank');
     },
   },
 };

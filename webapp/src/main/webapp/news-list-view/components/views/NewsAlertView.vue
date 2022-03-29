4fd2755889c38de7c4a2540ba19a5c90a817511d
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-03-29 11:59:50 +0100

    clean code 1

--- webapp/src/main/webapp/news-list-view/components/views/NewsAlertView.vue
+++ webapp/src/main/webapp/news-list-view/components/views/NewsAlertView.vue
@@ -1,8 +1,24 @@
+<!--
+Copyright (C) 2021 eXo Platform SAS.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+-->
 <template>
   <div id="critical-alert-ticker" :class="canPublishNews ? 'mt-n13' : 'mt-n5'">
     <div class="ticker-header">
       <div class="ticker-icon">
-        <v-icon style="color: white" >warning</v-icon>
+        <v-icon style="color: white">warning</v-icon>
       </div>
       <span v-if="!emptyTemplate">{{ $t('news.alerts.header') }}</span>
     </div>
@@ -28,7 +44,7 @@
                   :format="dateFormat" />
               </span>
               <span class="ticker-seperator">|</span>
-              <span class="ticker-articleTitle">{{item.title}} - {{item.summary}}</span>
+              <span class="ticker-articleTitle">{{ item.title }} - {{ item.summary }}</span>
             </div>
           </a>
         </v-carousel-item>
@@ -121,8 +137,6 @@ export default {
           .then(newsList => {
             this.news = newsList.news;
             this.initialized = true;
-            console.log(newsList.news);
-
           })
           .finally(() => this.initialized = false);
       }

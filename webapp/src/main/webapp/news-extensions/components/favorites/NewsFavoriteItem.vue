664c62cf7cabda420944d324656a32107469a350
saraboutej <sboutej@exoplatform.com>
2022-04-05 14:05:43 +0100

    Task-53938: Add news favorite extension

---
+++ webapp/src/main/webapp/news-extensions/components/favorites/NewsFavoriteItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-list-item class="clickable" :href="url">
+    <v-list-item-icon class="me-3 my-auto">
+      <v-icon size="22" class="icon-default-color"> fas fa-newspaper </v-icon>
+    </v-list-item-icon>
+
+    <v-list-item-content>
+      <v-list-item-title class="text-color body-2">{{ activityTitle }}</v-list-item-title>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <v-btn icon>
+        <v-icon class="yellow--text text--darken-2" size="18">fa-star</v-icon>
+      </v-btn>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: () => null,
+    },
+  },
+  data: () => ({
+    activityTitle: '',
+    url: ''
+  }),
+  created() {
+    this.$newsServices.getNewsById(this.id, false)
+      .then(news => {
+        this.activityTitle = news.title;
+        this.url = news.url;
+      });
+  },
+};
+</script>

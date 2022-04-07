27706d809a81e16b246c67532e8441cb0bd4a6cb
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-04-07 13:27:35 +0100

    get data from back side

--- webapp/src/main/webapp/news-list-view/components/views/NewsStoriesView.vue
+++ webapp/src/main/webapp/news-list-view/components/views/NewsStoriesView.vue
@@ -16,22 +16,124 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
   <div id="top-articles-horizontal-cards-2a">
-    <div id="cards-containera">
-      <div class="cardsa">
-        <card-carousel parent-class="activity-files-parent">
-          <news-stories-view-item
-            v-for="(item, index) in [0,0,0,0,0,0,0,0,0]"
-            :key="index"
-            :see-all-articles="index === [0,0,0,0,0,0,0,0,0].length -1"
-            class="activity-file-item" />
-        </card-carousel>
-      </div>
-    </div>
+    <card-carousel v-if="news.length">
+      <news-stories-view-item
+        v-for="(item, index) in news"
+        :key="index"
+        :item="item"
+        :selected-option="selectedOption" />
+      <a
+        class="see-all-link"
+        target="_self"
+        :href="seeAllArticles">
+        <div class="card" id="see-all">
+          <div class="see-all-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              version="1.1"
+              id="Layer_1"
+              x="0px"
+              y="0px"
+              viewBox="0 0 330 330"
+              style="enable-background:new 0 0 330 330;"
+              xml:space="preserve">
+              <path d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M85,190  c-13.785,0-25-11.215-25-25s11.215-25,25-25s25,11.215,25,25S98.785,190,85,190z M165,190c-13.785,0-25-11.215-25-25  s11.215-25,25-25s25,11.215,25,25S178.785,190,165,190z M245,190c-13.785,0-25-11.215-25-25s11.215-25,25-25  c13.785,0,25,11.215,25,25S258.785,190,245,190z"></path>
+            </svg>
+          </div>
+          <div class="see-all-text">See all articles</div>
+        </div>
+      </a>
+    </card-carousel>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    newsTarget: {
+      type: String,
+      required: false,
+      default: null
+    },
+  },
+  data () {
+    return {
+      news: [],
+      initialized: false,
+      limit: 10,
+      offset: 0,
 
+      showHeader: false,
+      showSeeAll: false,
+      showArticleTitle: true,
+      showArticleSummary: true,
+      showArticleImage: true,
+      showArticleAuthor: true,
+      showArticleSpace: true,
+      showArticleDate: true,
+      showArticleReactions: true,
+      seeAllUrl: '',
+      selectedOption: null,
+    };
+  },
+  created() {
+    this.reset();
+    this.$root.$on('saved-news-settings', this.refreshNewsViews);
+    this.getNewsList();
+  },
+  computed: {
+    seeAllArticles() {
+      return this.seeAllUrl && `${eXo.env.portal.context}/${eXo.env.portal.portalName}${this.seeAllUrl}`;
+    }
+  },
+  methods: {
+    getNewsList() {
+      if (!this.initialized) {
+        this.$newsListService.getNewsList(this.newsTarget, this.offset, this.limit, true)
+          .then(newsList => {
+            this.news = newsList.news;
+            this.initialized = true;
+          })
+          .finally(() => this.initialized = false);
+      }
+    },
+    refreshNewsViews(selectedTarget, selectedOption) {
+      this.showArticleSummary = selectedOption.showArticleSummary;
+      this.showArticleTitle = selectedOption.showArticleTitle;
+      this.showArticleImage = selectedOption.showArticleImage;
+      this.seeAllUrl = selectedOption.seeAllUrl;
+      this.limit = selectedOption.limit;
+      this.selectedOption = selectedOption;
+      this.newsTarget = selectedTarget;
+      this.getNewsList();
+    },
+    reset() {
+      this.limit = this.$root.limit;
+      this.showHeader = this.$root.showHeader;
+      this.showSeeAll = this.$root.showSeeAll;
+      this.showArticleTitle = this.$root.showArticleTitle;
+      this.showArticleImage = this.$root.showArticleImage;
+      this.showArticleSummary = this.$root.showArticleSummary;
+      this.showArticleAuthor = this.$root.showArticleAuthor;
+      this.showArticleSpace = this.$root.showArticleSpace;
+      this.showArticleDate = this.$root.showArticleDate;
+      this.showArticleReactions = this.$root.showArticleReactions;
+      this.seeAllUrl = this.$root.seeAllUrl;
+      this.selectedOption = {
+        limit: this.limit,
+        showHeader: this.showHeader,
+        showSeeAll: this.showSeeAll,
+        showArticleTitle: this.showArticleTitle,
+        showArticleSummary: this.showArticleSummary,
+        showArticleAuthor: this.showArticleAuthor,
+        showArticleSpace: this.showArticleSpace,
+        showArticleDate: this.showArticleDate,
+        showArticleReactions: this.showArticleReactions,
+        showArticleImage: this.showArticleImage,
+        seeAllUrl: this.seeAllUrl,
+      };
+    },
+  }
 };
 </script>
--- webapp/src/main/webapp/news-list-view/components/views/NewsStoriesViewItem.vue
+++ webapp/src/main/webapp/news-list-view/components/views/NewsStoriesViewItem.vue
@@ -15,113 +15,128 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <div class="card" id="card-$uuid">
+  <div class="card">
     <a
       class="articleLink"
       target="_self"
       href="$itemLink">
-      <img class="article-img" src="https://picsum.photos/500">
+      <img
+        class="article-img"
+        :src="articleImage"
+        :alt="$t('news.latest.alt.articleImage')">
       <div class="author-date-container">
-        <img class="author-photo" src="https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg">
-        <div class="date">Apr 17</div>
+        <img
+          class="author-photo"
+          :src="item.authorAvatarUrl"
+          alt="Author image">
+        <div v-if="showArticleDate" class="author-date">
+          <date-format
+            :value="displayDate"
+            :format="dateFormat" />
+        </div>
       </div>
     </a>
     <a
       class="articleLink"
       target="_self"
-      href="$itemLink">
+      :href="item.url">
       <div class="title-container">
-        <div class="article-title">All you need to know about the ACME group acquisition you need to know about the HG</div>
-        <div class="article-counters">
-          <div class="reactions">
-            <i class="likes-icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 489.543 489.543"
-                style="enable-background:new 0 0 489.543 489.543;"
-                xml:space="preserve">
-                <path d="M270.024,0c-22.6,0-15,48.3-15,48.3s-48.3,133.2-94.5,168.7c-9.9,10.4-16.1,21.9-20,31.3l0,0l0,0    c-0.9,2.3-1.7,4.5-2.4,6.5c-3.1,6.3-9.7,16-23.8,24.5l46.2,200.9c0,0,71.5,9.3,143.2,7.8c28.7,2.3,59.1,2.5,83.3-2.7    c82.2-17.5,61.6-74.8,61.6-74.8c44.3-33.3,19.1-74.9,19.1-74.9c39.4-41.1,0.7-75.6,0.7-75.6s21.3-33.2-6.2-58.3    c-34.3-31.4-127.4-10.5-127.4-10.5l0,0c-6.5,1.1-13.4,2.5-20.8,4.3c0,0-32.2,15,0-82.7C346.324,15.1,292.624,0,270.024,0z"></path>
-                <path d="M127.324,465.7l-35-166.3c-2-9.5-11.6-17.3-21.3-17.3h-66.8l-0.1,200.8h109.1C123.024,483,129.324,475.2,127.324,465.7z"></path>
-              </svg>
-            </i>
-            <div class="likes-count">25</div>
-            <i class="comments-icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                version="1.1"
-                id="Capa_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 60 60"
-                style="enable-background:new 0 0 60 60;"
-                xml:space="preserve">
-                <path d="M55.232,43.104C58.354,38.746,60,33.705,60,28.5c0-14.888-13.458-27-30-27S0,13.612,0,28.5s13.458,27,30,27  c4.262,0,8.378-0.79,12.244-2.348c6.805,3.927,16.212,5.282,16.618,5.338c0.046,0.007,0.093,0.01,0.139,0.01  c0.375,0,0.725-0.211,0.895-0.554c0.192-0.385,0.116-0.849-0.188-1.153C57.407,54.493,55.823,49.64,55.232,43.104z"></path>
-              </svg>
-            </i>
-            <div class="comments-count">12</div>
-            <div class="views">
-              <i class="views-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  version="1.1"
-                  id="Capa_1"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 488.85 488.85"
-                  style="enable-background:new 0 0 488.85 488.85;"
-                  xml:space="preserve">
-                  <path d="M244.425,98.725c-93.4,0-178.1,51.1-240.6,134.1c-5.1,6.8-5.1,16.3,0,23.1c62.5,83.1,147.2,134.2,240.6,134.2   s178.1-51.1,240.6-134.1c5.1-6.8,5.1-16.3,0-23.1C422.525,149.825,337.825,98.725,244.425,98.725z M251.125,347.025   c-62,3.9-113.2-47.2-109.3-109.3c3.2-51.2,44.7-92.7,95.9-95.9c62-3.9,113.2,47.2,109.3,109.3   C343.725,302.225,302.225,343.725,251.125,347.025z M248.025,299.625c-33.4,2.1-61-25.4-58.8-58.8c1.7-27.6,24.1-49.9,51.7-51.7   c33.4-2.1,61,25.4,58.8,58.8C297.925,275.625,275.525,297.925,248.025,299.625z"></path>
-                </svg>
-              </i>
-              <div class="views-count">333</div>
-            </div>
+        <div v-if="showArticleTitle" class="article-title">{{ item.title }}</div>
+        <div v-if="showArticleReactions" class="article-counters d-flex">
+          <div class="likes-container mb-1">
+            <v-icon class="counters-icons" size="14">mdi-thumb-up</v-icon>
+            <span class="counterStyle ml-1">{{ item.likesCount }}</span>
+          </div>
+          <div class="comments-container ml-2">
+            <v-icon
+              class="counters-icons mt-1"
+              size="14">
+              mdi-comment
+            </v-icon>
+            <span class="counterStyle ml-1">{{ item.commentsCount }}</span>
+          </div>
+          <div class="views-container ml-2">
+            <v-icon class="counters-icons" size="16">mdi-eye</v-icon>
+            <span class="counterStyle">{{ item.viewsCount }}</span>
           </div>
         </div>
       </div>
     </a>
-
-          
-    <div v-if="seeAllArticles">
-      <a
-        class="see-all-link"
-        target="_self"
-        href="/portal/dw/news">
-        <div class="card" id="see-all">
-          <div class="see-all-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              version="1.1"
-              id="Layer_1"
-              x="0px"
-              y="0px"
-              viewBox="0 0 330 330"
-              style="enable-background:new 0 0 330 330;"
-              xml:space="preserve">
-              <path d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M85,190  c-13.785,0-25-11.215-25-25s11.215-25,25-25s25,11.215,25,25S98.785,190,85,190z M165,190c-13.785,0-25-11.215-25-25  s11.215-25,25-25s25,11.215,25,25S178.785,190,165,190z M245,190c-13.785,0-25-11.215-25-25s11.215-25,25-25  c13.785,0,25,11.215,25,25S258.785,190,245,190z"></path>
-            </svg>
-          </div>
-          <div class="see-all-text">See all articles</div>
-        </div>
-      </a>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    seeAllArticles: {
-      type: Boolean,
-      default: false,
+    item: {
+      type: Object,
+      required: false,
+      default: null
+    },
+    selectedOption: {
+      type: Object,
+      required: false,
+      default: null
     },
   },
+  data: ()=> ({
+    dateFormat: {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    },
+    showArticleTitle: true,
+    showArticleSummary: true,
+    showArticleImage: true,
+    showArticleAuthor: true,
+    showArticleSpace: true,
+    showArticleDate: true,
+    showArticleReactions: true,
+  }),
+  computed: {
+    displayDate() {
+      return this.item.publishDate && this.item.publishDate.time && new Date(this.item.publishDate.time);
+    },
+    showArticleImage() {
+      return this.selectedOption && this.selectedOption.showArticleImage;
+    },
+    showArticleAuthor() {
+      return this.selectedOption && this.selectedOption.showArticleAuthor;
+    },
+    showArticleSpace() {
+      return this.selectedOption && this.selectedOption.showArticleSpace;
+    },
+    showArticleDate() {
+      return this.selectedOption && this.selectedOption.showArticleDate;
+    },
+    showArticleReactions() {
+      return this.selectedOption && this.selectedOption.showArticleReactions;
+    },
+    articleImage() {
+      return this.showArticleImage && this.item.illustrationURL !== null ? this.item.illustrationURL : '/news/images/news.png';
+    }
+  },
+  created() {
+    this.reset();
+  },
+  methods: {
+    reset() {
+      this.viewTemplate = this.$root.viewTemplate;
+      this.viewExtensions = this.$root.viewExtensions;
+      this.newsTarget = this.$root.newsTarget;
+      this.newsHeader = this.$root.header;
+      this.limit = this.$root.limit;
+      this.showHeader = this.$root.showHeader;
+      this.showSeeAll = this.$root.showSeeAll;
+      this.showArticleTitle = this.$root.showArticleTitle;
+      this.showArticleImage = this.$root.showArticleImage;
+      this.showArticleSummary = this.$root.showArticleSummary;
+      this.showArticleAuthor = this.$root.showArticleAuthor;
+      this.showArticleSpace = this.$root.showArticleSpace;
+      this.showArticleDate = this.$root.showArticleDate;
+      this.showArticleReactions = this.$root.showArticleReactions;
+      this.seeAllUrl = this.$root.seeAllUrl;
+    },
+  }
 };
 </script>

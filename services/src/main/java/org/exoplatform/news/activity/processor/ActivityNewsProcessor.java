6b3fca11c5d078dbc436ffb331ae15c7d6978733
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-05-16 16:37:31 +0100

    TASK-56521 Avoid duplicating Note Favorite & Tag Metadatas with Activity (#544)
    Prior to this change, when adding a news as favorite from Activity stream, a Favorite Metadata is associated to Activity and to the News at the same time (Same for tags). This fix will ensure to associate the favorite Metadata to the News content only. In fact, an improvement for Activity Metadata Management has been added that allows to add a specific target Object Type+Id when displaying activity in Stream. Thus, the Favorite button will manage the favorites of News Content Object identified by its real id.

--- services/src/main/java/org/exoplatform/news/activity/processor/ActivityNewsProcessor.java
+++ services/src/main/java/org/exoplatform/news/activity/processor/ActivityNewsProcessor.java
@@ -5,21 +5,26 @@ import java.util.HashMap;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.news.model.News;
 import org.exoplatform.news.service.NewsService;
+import org.exoplatform.news.utils.NewsUtils;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.services.security.ConversationState;
+import org.exoplatform.social.common.RealtimeListAccess;
 import org.exoplatform.social.core.BaseActivityProcessorPlugin;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.manager.ActivityManager;
 
 public class ActivityNewsProcessor extends BaseActivityProcessorPlugin {
 
-  private NewsService      newsService;
-
   private static final Log LOG = ExoLogger.getLogger(ActivityNewsProcessor.class);
 
-  public ActivityNewsProcessor(NewsService newsService, InitParams initParams) {
+  private NewsService      newsService;
+
+  private ActivityManager  activityManager;
+
+  public ActivityNewsProcessor(ActivityManager activityManager, NewsService newsService, InitParams initParams) {
     super(initParams);
     this.newsService = newsService;
+    this.activityManager = activityManager;
   }
 
   @Override
@@ -32,12 +37,17 @@ public class ActivityNewsProcessor extends BaseActivityProcessorPlugin {
     }
     News news = (News) activity.getLinkedProcessedEntities().get("news");
     if (news == null) {
-      org.exoplatform.services.security.Identity currentIdentity = ConversationState.getCurrent().getIdentity();
       try {
-        news = newsService.getNewsById(activity.getTemplateParams().get("newsId"), currentIdentity, false);
-      } catch (IllegalAccessException e) {
-        LOG.warn("User {} attempt to access unauthorized news with id {}",
-                 currentIdentity.getUserId(),
+        news = newsService.getNewsById(activity.getTemplateParams().get("newsId"), false);
+
+        RealtimeListAccess<ExoSocialActivity> listAccess = activityManager.getCommentsWithListAccess(activity, true);
+        news.setCommentsCount(listAccess.getSize());
+        news.setLikesCount(activity.getLikeIdentityIds() == null ? 0 : activity.getLikeIdentityIds().length);
+
+        activity.setMetadataObjectId(news.getId());
+        activity.setMetadataObjectType(NewsUtils.NEWS_METADATA_OBJECT_TYPE);
+      } catch (Exception e) {
+        LOG.warn("Error retrieving news with id {}",
                  activity.getTemplateParams().get("newsId"),
                  e);
       }

e04aa840413386f7d2569e27ab1196774d08321a
GouadriaHanen <hgouadria@exoplatform.com>
2022-11-01 17:57:42 +0100

    feat:fix java doc - EXO-60548

--- services/src/main/java/org/exoplatform/news/storage/jcr/JcrNewsStorage.java
+++ services/src/main/java/org/exoplatform/news/storage/jcr/JcrNewsStorage.java
@@ -323,7 +323,7 @@ public class JcrNewsStorage implements NewsStorage {
   /**
    * Publish a news
    *
-   * @param newsId The id of the news to be published
+   * @param news {@link News} The id of the news to be published
    * @throws Exception when error
    */
   public void publishNews(News news) throws Exception {

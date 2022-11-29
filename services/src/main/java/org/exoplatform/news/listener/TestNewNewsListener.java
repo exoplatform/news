c02bd37c8ed2b1f7dcef08a88d326b8a49527753
Romain Dénarié <romain.denarie@exoplatform.com>
2022-11-29 11:52:57 +0100

    Test header rules

---
+++ services/src/main/java/org/exoplatform/news/listener/TestNewNewsListener.java
@@ -0,0 +1,20 @@
+package org.exoplatform.news.listener;
+
+import org.exoplatform.news.model.News;
+import org.exoplatform.services.listener.Asynchronous;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+@Asynchronous
+public class TestNewNewsListener extends Listener<String, News> {
+
+  private static final Log LOG =
+      ExoLogger.getLogger(TestNewNewsListener.class);
+
+  @Override
+  public void onEvent(Event<String, News> event) throws Exception {
+    LOG.info("Start event");
+  }
+
+}
---
+++ webapp/src/main/webapp/latest-news/components/NewExoLatestNews.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app
+    class="VuetifyApp"
+    flat>
+  </v-app>
+</template>
+

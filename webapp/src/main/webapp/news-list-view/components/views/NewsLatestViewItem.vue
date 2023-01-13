de4f086dea0595c4635f39de21d0d9e0f9e722ea
Anwar khanfir <akhanfir@exoplatform.com>
2023-01-13 10:16:59 +0100

    fix: News template headlines+list: Move container to side layout output issue. - EXO-61647 Prior this change, when move the headlines news template on side layout, The styles has changed : the title isn't extended on 2 lines, the space and news title on main article are in black. After this change , The same style in main container maintained.

--- webapp/src/main/webapp/news-list-view/components/views/NewsLatestViewItem.vue
+++ webapp/src/main/webapp/news-list-view/components/views/NewsLatestViewItem.vue
@@ -30,9 +30,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
           class="spaceImage"
           :src="item.spaceAvatarUrl"
           :alt="$t('news.latest.alt.spaceImage')">
-        <span class="spaceName text-body-1">{{ item.spaceDisplayName }}</span>
+        <span class="spaceName text-color text-body-1">{{ item.spaceDisplayName }}</span>
       </div>
-      <span v-if="showArticleTitle" class="articleTitle text-body-1">{{ item.title }}</span>
+      <span v-if="showArticleTitle" class="articleTitle text-color text-body-1">{{ item.title }}</span>
       <div class="articlePostTitle">
         <div class="reactions">
           <v-icon

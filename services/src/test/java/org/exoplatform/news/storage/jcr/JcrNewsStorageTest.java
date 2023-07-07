f32f4ed125bd582ea2e4b12b3bfb5896ddba4ba8
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2023-07-07 12:15:26 +0100

    fix : enable to display published news images selected from existing upload for non members - EXO-65111 (#875)

--- services/src/test/java/org/exoplatform/news/storage/jcr/JcrNewsStorageTest.java
+++ services/src/test/java/org/exoplatform/news/storage/jcr/JcrNewsStorageTest.java
@@ -162,7 +162,6 @@ public class JcrNewsStorageTest {
   public static void afterRunBare() throws Exception { // NOSONAR
     COMMONS_UTILS.close();
     PORTAL_CONTAINER.close();
-    REST_UTILS.close();
   }
 
   @Test

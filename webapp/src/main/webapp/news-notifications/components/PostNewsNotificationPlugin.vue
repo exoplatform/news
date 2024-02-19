445939d30af0ba0817d034a612267a13a74e129d
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2024-02-19 15:24:15 +0100

    fix: Can't access to published news through notification - EXO-69364
    Before this change, when user who is not member in spacex receive the notification has published the article: news1 in spacex and click on it, user redirected to activity id of the news. After this change, as the user is not a member in the space he is redirected to the news id (news details).
    
    (cherry picked from commit 1bc7d05b11d6646169632f580cf34bfa891548bc)

--- webapp/src/main/webapp/news-notifications/components/PostNewsNotificationPlugin.vue
+++ webapp/src/main/webapp/news-notifications/components/PostNewsNotificationPlugin.vue
@@ -40,7 +40,8 @@ export default {
   },
   computed: {
     url() {
-      return this.notification?.parameters?.ACTIVITY_LINK;
+      return this.notification?.space?.isMember ? this.notification?.parameters?.ACTIVITY_LINK
+        : `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/news/detail?newsId=${this.notification?.parameters?.NEWS_ID}`;
     },
     eventTitle() {
       return this.notification?.parameters?.CONTENT_TITLE;

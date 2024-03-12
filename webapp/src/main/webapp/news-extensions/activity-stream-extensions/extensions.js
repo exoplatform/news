f401b56f13487dfc9d0aa1e29d1630ec9b5ade55
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2024-03-12 12:02:12 +0100

    feat: Remove NewsExtensions portlet from bottom-all-container configuration - EXO-69984 (#1130)
    Prior to this change, in bottom-all-container, we inject and load news extensions as a portlet, so, it is loaded on all pages even if no need to be used which can lead to loading performance issues.
    To fix that, we must use js load-group, which are now available through all webapps to load news extensions only with the portlets of the same load-group (example activity stream, notification, analytics table, favorite drawer).

--- webapp/src/main/webapp/news-extensions/activity-stream-extensions/extensions.js
+++ webapp/src/main/webapp/news-extensions/activity-stream-extensions/extensions.js
@@ -13,6 +13,12 @@
   You should have received a copy of the GNU Affero General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
+const lang = eXo.env.portal.language || 'en';
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.news.News-${lang}.json`;
+
+const i18nPromise = exoi18n.loadLanguageAsync(lang, url).then(i18n => new Vue({i18n}));
+
 const newsActivityTypeExtensionOptions = {
   name: 'News',
   getExtendedComponent: (activity, isActivityDetail) => {
@@ -73,4 +79,27 @@ export function initExtensions() {
     type: 'news',
     options: newsActivityTypeExtensionOptions,
   });
+  if (eXo.env.portal.spaceId) {
+    Vue.prototype.$newsServices.canUserCreateNews(eXo.env.portal.spaceId).then(canCreateNews => {
+      if (canCreateNews) {
+        return i18nPromise.then(() => {
+          extensionRegistry.registerComponent('ActivityComposerAction', 'activity-composer-action', {
+            id: 'switchNewsButton',
+            vueComponent: Vue.options.components['activity-switch-to-news'],
+            rank: 10,
+          });
+          extensionRegistry.registerComponent('ActivityComposerFooterAction', 'activity-composer-footer-action', {
+            id: 'writeNewsButton',
+            vueComponent: Vue.options.components['activity-write-news-composer'],
+            rank: 30,
+          });
+          extensionRegistry.registerComponent('ActivityToolbarAction', 'activity-toolbar-action', {
+            id: 'writeNewsToolbarButton',
+            vueComponent: Vue.options.components['activity-write-news-toolbar-action'],
+            rank: 30,
+          });
+        });
+      }
+    });
+  }
 }

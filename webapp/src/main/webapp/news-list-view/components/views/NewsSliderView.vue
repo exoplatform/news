006879d49aea6a08599e4d6f29909e4180bc8345
jihed525 <jchabbeh@exoplatform.com>
2024-11-01 12:05:49 +0100

    fix: News Slider - Mouse hover makes article content misplaced - EXO-75170 - Meeds-io/meeds#255

--- webapp/src/main/webapp/news-list-view/components/views/NewsSliderView.vue
+++ webapp/src/main/webapp/news-list-view/components/views/NewsSliderView.vue
@@ -50,7 +50,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
                   v-if="showArticleTitle"
                   :href="articleUrl(item)"
                   class="flex flex-row flex-grow-1 align-center justify-center headLinesTruncate"
-                  :class="extraClass.concat(canPublishNews && !hover ? 'mt-12' : '')">
+                  :class="extraClass.concat(canPublishNews ? 'mt-12' : '')">
               <span class="text-h4 font-weight-medium white--text text-truncate-2">
                 {{ item.title }}
               </span>
